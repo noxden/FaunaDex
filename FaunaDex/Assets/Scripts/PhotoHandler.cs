9ed12ba6ba5b359d83bac0e7b29d5a2d87ffee2f
Max von Trümbach <max.vontruembach@3spin.com>
2023-01-22 17:50:51 +0100

    Faster Screenshots and only takes screenshot when Raycast hits

--- FaunaDex/Assets/Scripts/PhotoHandler.cs
+++ FaunaDex/Assets/Scripts/PhotoHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using Image = UnityEngine.UI.Image;
 
@@ -6,31 +7,48 @@ public class PhotoHandler : MonoBehaviour
 {
     public Camera Camera;
     public Canvas Canvas;
+    public GameObject PolaroidObject;
+    public GameObject ScreenshotFlash;
     
     private Vector2 CamCenter { get; set; }
     private GameObject Polaroid { get; set; }
     private GameObject ScreenshotObject { get; set; }
     private Texture2D ScreenshotTexture { get; set; }
+    
+    private Ray Raycaster { get; set; }
     public void CreateCube()
     {
-        if (Polaroid == null)
+        if(Polaroid != null && Polaroid.activeSelf)
         {
-            Polaroid = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            //Instantiate(new GameObject("Polaroid"));
+            Polaroid.SetActive(false);
+            //DestroyImmediate(Polaroid, true);
+        }
+        else if (Polaroid != null && !Polaroid.activeSelf) 
+        {
+            Polaroid.SetActive(true);
+        }
+        else if (Polaroid == null)
+        {
+            int RaycastTarget = LayerMask.NameToLayer("RaycastTarget");
+            Polaroid = Instantiate(PolaroidObject);
+            Polaroid.layer = RaycastTarget;
 
             Polaroid.transform.position =
                 new Vector3(CamCenter.x - 0.5f, CamCenter.y - 0.5f, Camera.transform.position.z + 4);
         }
-        else
-        {
-            Destroy(Polaroid);
-        }
+        
         
     }
 
-    public async void TakeScreenshot()
+    public void TakeScreenshot()
     {
-        ScreenCapture.CaptureScreenshot("Assets/Resources/Screenshot.png");
+
+        if (Physics.Raycast(Raycaster, out _,Mathf.Infinity, LayerMask.GetMask("RaycastTarget")))
+        {
+            Debug.Log("Hit");
+            ScreenshotTexture = ScreenCapture.CaptureScreenshotAsTexture();
+            StartCoroutine(ScreenshotFlashing());
+        }
     }
 
     public void OpenScreenshot()
@@ -39,24 +57,35 @@ public class PhotoHandler : MonoBehaviour
         {
             Destroy(ScreenshotObject);
         }
-        ScreenshotTexture = Resources.Load<Texture2D>("Screenshot");
 
         if (ScreenshotTexture != null)
         {
             ScreenshotObject = Instantiate(new GameObject("Screenshot",typeof(RectTransform),typeof(CanvasRenderer),typeof(Image)),Canvas.transform);
             Sprite sprite = Sprite.Create(ScreenshotTexture, new Rect(0,0, ScreenshotTexture.width,ScreenshotTexture.height), new Vector2(0.5f, 0.5f));
-            
+            ScreenshotObject.GetComponent<RectTransform>().sizeDelta =
+                new Vector2(ScreenshotTexture.width, ScreenshotTexture.height);
+            ScreenshotObject.GetComponent<RectTransform>().localScale = new Vector3(0.3f, 0.3f, 0.3f);
             ScreenshotObject.GetComponent<Image>().sprite = sprite; 
         }
     }
     void Update()
     {
+        
+        Raycaster = new Ray(CamCenter, Camera.transform.forward);
         CamCenter = Camera.rect.center;
+        Debug.DrawRay(Camera.transform.position, Camera.transform.forward, Color.green);
 
         if (Polaroid != null)
         {
             Polaroid.transform.position =
                 new Vector3(CamCenter.x - 0.5f, CamCenter.y - 0.5f, Camera.transform.position.z + 4);
         }
+    }
+
+    private IEnumerator ScreenshotFlashing()
+    {
+        ScreenshotFlash.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        ScreenshotFlash.SetActive(false);
     }
 }
