79c54a4d0aae275b33fd41aa49915ced22a06116
Daniel Heilmann <Daniel.Heilmann@stud.h-da.de>
2023-01-30 22:22:13 +0100

    Updated timestamp on LocationServiceHandler

--- FaunaDex/Assets/Scripts/MapScene/LocationServiceHandler.cs
+++ FaunaDex/Assets/Scripts/MapScene/LocationServiceHandler.cs
@@ -2,7 +2,7 @@
 // Darmstadt University of Applied Sciences, Expanded Realities
 // Course:       Project 5 (Grimm, Hausmeier, Vollert)
 // Script by:    Daniel Heilmann (771144)
-// Last changed: 24-01-23
+// Last changed: 27-01-23
 //================================================================
 
 using System.Collections;
