7d03737bc11a417385633ed463ec8a24eca9725b
Daniel Heilmann <Daniel.Heilmann@stud.h-da.de>
2023-01-31 23:02:27 +0100

    [Cleanup] Added script headers

--- FaunaDex/Assets/Scripts/DialogueEntry.cs
+++ FaunaDex/Assets/Scripts/DialogueEntry.cs
@@ -1,3 +1,10 @@
+//================================================================
+// Darmstadt University of Applied Sciences, Expanded Realities
+// Course:       Project 5 (Grimm, Hausmeier, Vollert)
+// Script by:    Daniel Heilmann (771144)
+// Last changed: 31-01-23
+//================================================================
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
