2b2f1a10be6ab0fa435cb39b9a553067c1e8458c
Daniel Heilmann <Daniel.Heilmann@stud.h-da.de>
2023-02-01 18:29:41 +0100

    Implemented QuestStage-based sprite changes and expanded VPSArea class
    - The VPSArea class now also holds information about at which quest stage it should be visible on the map

--- FaunaDex/Assets/Scripts/VPSArea.cs
+++ FaunaDex/Assets/Scripts/VPSArea.cs
@@ -12,4 +12,52 @@ using UnityEngine;
 public class VPSArea : MonoBehaviour
 {
     public Scene linkedScene;
+
+    [SerializeField]
+    private int visibleAtQuestStage;
+
+    [Tooltip("Only for visualization, do not touch!")]
+    [SerializeField]
+    private List<GameObject> children;
+
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+            children.Add(child);
+        }
+
+        CheckQuestStage();
+    }
+
+    public void OnQuestUpdated(int newQuestState)
+    {
+        Debug.Log($"Received Quest Update on {this.name}.");
+        CheckQuestStage();
+    }
+
+    private void CheckQuestStage()
+    {
+        if (PersistentSaveData.instance.questStage_Otto < visibleAtQuestStage)
+            Hide();
+        else
+            Show();
+    }
+
+    private void Hide()
+    {
+        foreach (GameObject child in children)
+        {
+            child.SetActive(false);
+        }
+    }
+
+    private void Show()
+    {
+        foreach (GameObject child in children)
+        {
+            child.SetActive(true);
+        }
+    }
 }
